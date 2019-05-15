833a06569f81cbebdfe10757a26fa7b25002d4dc
Rohit Pratti <rohit.pratti@gmail.com>
2019-05-15 03:03:52 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,32 @@
+def fizzbuzz(num)
+
+if (num%3 == 0 && num%5 == 0){
+  
+  return "FizzBuzz"
+  
+}
+
+elsif (num%3 == 0) {
+  
+  return "Fizz"
+  
+} 
+
+elsif (num%5 == 0) {
+  
+  "Buzz"
+  
+}
+
+else {
+  
+   nil
+  
+}
+
+end
+
+
+
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
